94de4e20b9b55f9bfc3794d863694142409189b5
taznimt <taznimanwar06@gmail.com>
2025-10-28 10:26:28 +0700

    add GET /komik endpoint to retrieve all Komik data

--- index.js
+++ index.js
@@ -30,6 +30,15 @@ app.post("/komik", async (req, res)=> {
     }
 
 });
+app.get('/komik', async (req, res)=> {
+    try {
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
 
 
 
